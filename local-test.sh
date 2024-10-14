c051d76e6695e5b10f796b97383041e9dea59e7d
ncc111 <39723749+ncc111@users.noreply.github.com>
2024-10-14 21:42:43 +0800

    Update local-test.sh

--- local-test.sh
+++ local-test.sh
@@ -1,3 +1,15 @@
-DATE=`date +%F`
-echo "Built on: " > /tmp/buildhook.log
-echo $DATE >> /tmp/buildhook.log
+#!/bin/bash
+
+# Name of the log file where script output will be saved
+LOGFILE="/var/log/build-post-commit.log"
+
+# Print the current date and time to the log file
+echo "Build post-commit hook executed on $(date)" >> $LOGFILE
+
+# Check if the application directory exists
+if [ -d "/opt/app-root/src" ]; then
+    echo "Application directory exists." >> $LOGFILE
+else
+    echo "Application directory does not exist. Exiting." >> $LOGFILE
+    exit 1
+fi
